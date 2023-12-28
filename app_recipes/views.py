ac2025b00a4af9f55bc4cc6bcf64290a7af1e9a2
KaueTribess <kaueirontribess@gmail.com>
2023-12-28 10:47:59 -0300

    Splited the head from the home.html

--- app_recipes/views.py
+++ app_recipes/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return render(request=request, template_name='recipes/home.html', context={'name': 'Kauê Tribess'})
+    return render(request=request, template_name='recipes/pages/home.html', context={'name': 'Kauê Tribess'})
